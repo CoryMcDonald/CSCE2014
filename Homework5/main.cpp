350161b36b4565b8767d42ede7f119d3b172a161
CoryMcDonald <cory@corywmcdonald.com>
2013-12-09 21:28:09 -0600

    Implemented multiexpressions

--- Homework5/main.cpp
+++ Homework5/main.cpp
@@ -22,24 +22,32 @@ string replaceInString(string originalString, string toReplace)
     {
         string variableReplacing = toReplace.substr(0,1);
         string valueToReplaceWith = toReplace.substr(2,toReplace.size());
-        newString = originalString.replace(originalString.find(variableReplacing),
-                valueToReplaceWith.size(), valueToReplaceWith);
+        if(originalString.find(variableReplacing) != -1)
+        {
+                newString = originalString.replace(originalString.find(variableReplacing),
+                        valueToReplaceWith.size(), valueToReplaceWith);
+        }
     }else
     {
-        newString = originalString.replace(originalString.find(toReplace), 0, "");
+        if(originalString.find(toReplace) != -1)
+        {
+                newString = originalString.replace(originalString.find(toReplace), 1, " ");
+        }else {
+            newString = originalString;
+        }
     }
     return newString;
 }
 
 int main(int argc, char** argv) {
-    string input ="";
-    //string input = "a+b-c*d/e;a=1;b=2;c=3;d=4;e=5;"; 
-    //string input = "(2+3)*4;";
-    //string input = "2+3*4;";
+    //string input ="";
+    string input = "a+b-c*d/e;a=1;b=2;c=3;d=4;e=5;"; 
+    //string input = "(2+3)*4;2+3*4;";
     //string input = "2+3;";
     string action = "=";
     string additionalInput = "";
     string originalExpression = "";
+    string stringToReplace;
     bool continueExecution = true;
     Expression inputExpression;
     char * expressionToEvaluate;
@@ -57,60 +65,91 @@ int main(int argc, char** argv) {
         cout << "Action:";        
         cin >> action;
         
-        expressionToEvaluate = strdup(input.c_str());
-        char * nextString;
-        nextString = strtok (expressionToEvaluate, ";");
-        originalExpression= string(expressionToEvaluate);
-
-        inputExpression.tokenized.clear();
-        inputExpression.set(originalExpression);
+        //todo clean up the code below
+        //todo Add additional comments so you can follow what i'm doing
+        //todo implement the parenthesis output
+        //todo add error checking and try catch 
         
-        switch(tolower(action.c_str()[0]))
+        if(action[0] == '=' || action[0] == '<' || action[0] == '>')
         {
-            case '=' :                             
-
-                for(int i=0; nextString != NULL; i++)
-                {      
-                    if(i != 0)
+            expressionToEvaluate = strdup(input.c_str());
+            char * nextString;
+            nextString = strtok (expressionToEvaluate, ";");
+            originalExpression= string(expressionToEvaluate);
+            stringToReplace = originalExpression;
+            for(int i=0; nextString != NULL; i++)
+            {  
+                if(input.find("=") == -1)
+                {
+                    expressionToEvaluate = strdup(replaceInString(nextString, ";").c_str());
+                }else
+                {
+                    if(i > 0)
                     {
-                        expressionToEvaluate = strdup(replaceInString(expressionToEvaluate, nextString).c_str());
+                        expressionToEvaluate = strdup(replaceInString(expressionToEvaluate, nextString).c_str());  
                     }
-                    nextString = strtok(NULL, ";");                    
-                }
+                }             
                 
                 inputExpression.tokenized.clear();
                 inputExpression.set(expressionToEvaluate);
-                cout << originalExpression << " = " 
-                       << inputExpression.evaluateExpression() << endl;
-                break;
-            case '>' :
-                cout << "Prefix of " << originalExpression << " is: " 
-                       << inputExpression.convertToPreFix() << endl;
-                break;
-            case '<' :
-                cout << "Postfix of " << originalExpression << " is: " 
-                       << inputExpression.getPostfixString() << endl;
-                break;
-            case 'f' :
-                //todo convert each expression in the sequence of expressions to the equivalent
-                //fully parenthesized expression.
-                break;
-            case 'c' :
-                do{                
-                    cout <<"Additional Input: ";
-                    cin >> additionalInput;
-                }while(additionalInput.find(";") == -1);        
-                input += additionalInput;
-                break;
-            case 'q' :
-                    continueExecution = false;
+
+                nextString = strtok(NULL, ";"); 
+                
+                switch(tolower(action.c_str()[0]))
+                {
+                    case '=' :   
+                        if(input.find("=") == -1)
+                        {
+                            cout << expressionToEvaluate << " = " 
+                              << inputExpression.evaluateExpression() << endl;
+                        }else
+                        {
+                            if(nextString == NULL)
+                            {
+                                
+                                 cout << originalExpression << " = " 
+                                        << inputExpression.evaluateExpression() << endl;
+                            }
+                        }
+                        break;
+                    case '>' :
+                        cout << "Prefix of " << originalExpression << " is: " 
+                               << inputExpression.convertToPreFix() << endl;
+                        break;
+                    case '<' :
+                        cout << "Postfix of " << originalExpression << " is: " 
+                               << inputExpression.getPostfixString() << endl;
+                        break;
+                    default : break;
+                } 
+            }
+                            continueExecution = false;
+
+        }else
+        {
+            switch(tolower(action.c_str()[0]))
+            {
+                case 'f' :
+                    //todo convert each expression in the sequence of expressions to the equivalent
+                    //fully parenthesized expression.
                     break;
-            case 's' :
-                    input = "";
+                case 'c' :
+                    do{                
+                        cout <<"Additional Input: ";
+                        cin >> additionalInput;
+                    }while(additionalInput.find(";") == -1);        
+                    input += additionalInput;
                     break;
-            default :                
-                cout << "Invalid action" << endl;                
-                break;      
+                case 'q' :
+                        continueExecution = false;
+                        break;
+                case 's' :
+                        input = "";
+                        break;
+                default :                
+                    cout << "Invalid action" << endl;                
+                    break;      
+            }
         }
     }while (continueExecution);
     
